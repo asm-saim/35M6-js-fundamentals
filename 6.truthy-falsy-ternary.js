a3e887597ad9dea46deba30b7a0d4d3e5b00cbcd
asm-saim <saim777m@gmail.com>
2026-06-26 15:21:20 +0600

    type coercion explained and importance of == & ===

--- 6.truthy-falsy-ternary.js
+++ 6.truthy-falsy-ternary.js
@@ -17,3 +17,40 @@ else {
 }
 //using ternary:
 value ? console.log("It's falsy") : console.log("It's truthy")
+
+//Ex:
+const number = 30;
+const result = (number >= 10) && (number <= 15) ? "Low" : "High"
+console.log(result);
+
+// EX:1
+const isActive = true;
+
+const matched = () => console.log("Green Light")
+const unmatched = () => console.log("Red Light")
+isActive ? matched() : unmatched();
+
+//shortcut:
+
+//if truthy
+isActive && matched(); //no answer is falsy
+
+//if falsy:
+isActive || unmatched(); //no answer if truthy
+
+
+//Type coercion :
+// Type coercion is the process where JavaScript automatically converts one data type to another when performing an operation.
+
+let num = "10"
+num = -num - 2; //automatic it will convert to number type.
+console.log(typeof num);
+
+//Ex:
+console.log("5" + 2); //convert to a string
+console.log("5" - 2); //convert to a number
+console.log("5" / 2); //convert to a number
+console.log("5" * 2); //convert to a number
+
+// Tip: In modern JavaScript, prefer === over == to avoid unexpected type coercion during comparisons.
+
